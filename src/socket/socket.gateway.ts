5a528e66c5b4327a23bcd084f39c4683c902adc3
MaiT <maitmus@gmail.com>
2024-04-13 22:44:40 +0900

    삭제한 DTO 불러오기 수정

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -4,8 +4,6 @@ import {
   MessageBody,
 } from '@nestjs/websockets';
 import { SocketService } from './socket.service';
-import { CreateSocketDto } from './dto/create-socket.dto';
-import { UpdateSocketDto } from './dto/update-socket.dto';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSocketDto } from './dto/create-socket.dto';
-import { UpdateSocketDto } from './dto/update-socket.dto';
 
 @Injectable()
 export class SocketService {}
