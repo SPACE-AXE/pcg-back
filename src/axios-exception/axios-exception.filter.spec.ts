6101c74073b6a478fc53712787c860336060a684
MaiT <maitmus@gmail.com>
2024-05-10 14:36:13 +0900

    Feat. Axios 에러 전역 핸들링을 위한 Global Filter 추가

---
+++ src/axios-exception/axios-exception.filter.spec.ts
@@ -0,0 +1,7 @@
+import { AxiosExceptionFilter } from './axios-exception.filter';
+
+describe('AxiosExceptionFilter', () => {
+  it('should be defined', () => {
+    expect(new AxiosExceptionFilter()).toBeDefined();
+  });
+});
--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,17 +1,7 @@
-import {
-  WebSocketGateway,
-  SubscribeMessage,
-  MessageBody,
-} from '@nestjs/websockets';
+import { WebSocketGateway } from '@nestjs/websockets';
 import { SocketService } from './socket.service';
-import { UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
-@WebSocketGateway({
-  cookie: true,
-  cors: true,
-  transports: ['websocket'],
-})
+@WebSocketGateway({ cookie: true, cors: true, transports: ['websocket'] })
 export class SocketGateway {
   constructor(private readonly socketService: SocketService) {}
 }
