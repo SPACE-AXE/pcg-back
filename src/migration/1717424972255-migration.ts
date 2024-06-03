e9bced4cefa45782ca7a2d07035251806d0c9ccf
MaiT <maitmus@gmail.com>
2024-06-03 23:36:54 +0900

    Fix. minor patches

--- src/migration/1717424972255-migration.ts
+++ src/migration/1717424972255-migration.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1717424972255 implements MigrationInterface {
-    name = 'Migration1717424972255'
+  name = 'Migration1717424972255';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`birth\``);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`birth\``);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` ADD \`birth\` date NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`user\` ADD \`birth\` date NOT NULL`);
+  }
 }
