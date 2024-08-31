616ee8dc5ccc48d4b600f72cd126d0a11a808d69
MaiT <maitmus@gmail.com>
2024-09-01 03:36:20 +0900

    Feat. Request / Response에 대한 logger 추가

---
+++ src/app-logger/app-logger.middleware.ts
@@ -0,0 +1,30 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class AppLoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const logger = new Logger();
+    const { ip, method, path: url } = req;
+    const userAgent = req.get('user-agent') || '';
+
+    const startTime = Date.now();
+    logger.log(
+      `${method} ${url} from IP: ${ip}, User-Agent: ${userAgent}`,
+      'Request',
+    );
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('content-length') || 'N/A';
+      const responseTime = Date.now() - startTime;
+
+      logger.log(
+        `${method} ${url} ${statusCode} - ${contentLength} bytes - ${responseTime}ms`,
+        'Response',
+      );
+    });
+
+    next();
+  }
+}
